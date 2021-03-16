3ddb7bcfb0d1f2102e02c54b5360e8063868e898
[svijasvg <[login-en@svija.love]>
2021-03-16 17:39:20 +0100

    researching keyboard capture

--- host/index.jsx
+++ host/index.jsx
@@ -3,3 +3,11 @@ function test(arg){
 //  var fileRef = new File("~/Downloads/kobe.jpg");
 //  var docRef = app.open(fileRef);
 }
+
+//window.addEventListener("keydown", windowKeyDown);
+
+// function windowKeyDown(event){
+//     alert('window KeyHandler: '+event);
+// //    CodeMirror.signal(editor,'catchPanelKeyDown',event);
+// }
+
