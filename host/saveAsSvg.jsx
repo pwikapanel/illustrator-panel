c96d3be183d97f3ab7baca8987fdfc7213ac4c4f
[svijasvg <[login-en@svija.love]>
2021-03-24 14:56:25 +0100

    all working except name of single artboard svg

--- host/saveAsSvg.jsx
+++ host/saveAsSvg.jsx
@@ -6,116 +6,81 @@
 
 //———————————————————————————————————————— program
 
-//  if (app.documents.length > 0 ) {
-
+var d = new Date(); var ms = d.getTime();
 var activeDoc = app.activeDocument;
 var aiOptions = st_optionsForVersion(17);
-var docList = app.documents;
-var filesTreated = 0;
+var appDocs   = app.documents;
 
-for (x=0; x<app.documents.length; x++){
-  filesTreated += 1;
+for (index=0; index<app.documents.length; index++){
 
-  var sourceDoc = docList[x]; // ISG251
-  app.activeDocument = sourceDoc; // otherwise properties are not updated correctly
-  var realPath = sourceDoc.path + '/' + sourceDoc.name;
+  app.activeDocument = appDocs[index]; // ISG251
+  var sourceDoc = app.activeDocument;   
 
-  var boardsToRestore = st_saveAsSvgs(sourceDoc);
+  //———————————————————————————————— save state
 
-alert('artboards on 25: '+sourceDoc.artboards.length);
+  var realPath    = sourceDoc.path + '/' + sourceDoc.name;
+  var activeBoard = sourceDoc.artboards.getActiveArtboardIndex();
 
-  var destFile = new File(realPath);
+  //———————————————————————————————— save SVG's then Illustrator
 
-alert('artboards on 27: '+sourceDoc.artboards.length);
-  sourceDoc.saveAs(destFile, aiOptions); // adds artboards back
+  st_saveAsSvgs(sourceDoc);
+  var aiFile = new File(realPath);
+  sourceDoc.saveAs(aiFile, aiOptions);
 
-  alert('artboards on 30: '+sourceDoc.artboards.length);
   //————— housekeeping after SVG export
 
+  sourceDoc.artboards.setActiveArtboardIndex(activeBoard);
   if (arg == 'save') break;
   if (arg == 'close') sourceDoc.close(SaveOptions.DONOTSAVECHANGES);
+
 }
 
-app.activeDocument.artboards.add([0,0,100,-100]);
-alert('artboards on 37: '+app.activeDocument.artboards.length);
 if (arg != 'close') app.activeDocument = activeDoc;
 
-if (filesTreated < 2) var msg = 'File saved.';
+var d = new Date(); ms = d.getTime() - ms;
+if (index < 2) var msg = 'File saved.';
 else var msg = 'Files saved.';
-alert(msg);
-
+alert(msg + ' ('+ms+' ms)');
 
 // — — — — — — — — — — — — — — — — — — — — functions — — — — — — — — —
 
 //———————————————————————————————————————— save the actual SVG's
 
-see isg.169, activeartboardindex
+function st_saveAsSvgs(doc){
 
-I have a suspicion, that as a document has to be active to be maniuputaed,
-and artboard must be activated to be manipulated
-could potentially even active each artboard in turn, then save as svg
-and everything will take care of itself.
+  var destName   = doc.name.slice(0, -3);
+  var boardsLen  = doc.artboards.length;
+  var layersLen  = doc.layers.length;
 
-function st_saveAsSvgs(source){
+  //———————————————————————————————— delete unneeded layers
 
-  var folder = Folder(app.activeDocument.path);
-  var destName = source.name.slice(0, -3);
-  var boardsLen = source.artboards.length;
-  var layersLen  = source.layers.length;
-  var backupBoards = [];
-
-  for (t=0; t<boardsLen; t++) backupBoards[t] = st_copyArtboard(source.artboards[t]);
-
-  for (t=boardsLen-1; t>0; t--) source.artboards[t].remove();
+  // array w/ information about locked & visible
+  var backupLayers = st_deleteNonPrintingLayers(doc);
 
   for (j=0; j<boardsLen; j++){
-
-    //———————————————————————————————— configure artboard
-
-    var bb = backupBoards[j];
-    alert('69: restoring artboard '+bb['name']);
-    for (var key in bb) source.artboards[0][key] = bb[key];
-
-    //———————————————————————————————— delete unneeded layers
-
-    // array w/ information about locked & visible
-    var backupLayers = st_deleteNonPrintingLayers(sourceDoc);
+    doc.artboards.setActiveArtboardIndex(j);
 
     //———————————————————————————————— save svg file
+    // need to take into account a single artboard
 
-    var abName = source.artboards[0].name;
-    var finalName = destName + '-' + abName + '.svg';
+    var finalName = destName + '-' + doc.artboards[j].name + '.svg';
     var options  = st_getSvgOptions();
     var destFile = st_newFile(finalName);
+    doc.exportFile(destFile, ExportType.SVG, options);
 
     //———————————————————————————————— restore to original state
 
-    // restore layers & artboards
-
-    source.exportFile(destFile, ExportType.SVG, options);
-
-    while (source.layers.length<layersLen) app.undo();
+    // restore layers
+    while (doc.layers.length<layersLen) app.undo();
 
     // restore visibile & locked values
     for (var r=0; r<layersLen; r++){
-      if (backupLayers[r] == 1 || backupLayers[r] == 3){source.layers[r].locked  = true; }
-      if (backupLayers[r] == 2 || backupLayers[r] == 3){source.layers[r].visible = false;}
+      if (backupLayers[r] == 1 || backupLayers[r] == 3){doc.layers[r].locked  = true; }
+      if (backupLayers[r] == 2 || backupLayers[r] == 3){doc.layers[r].visible = false;}
     }
   }
 
-  return backupBoards;
-}
-
-//———————————————————————————————————————— copy an array key by key, skipping objects (parent)
-
-function st_copyArtboard(src){
-  var result = {};
-  for (var key in src){
-    if (key != 'parent'){
-      result[key] = src[key];
-    }
-  }
-  return result;
+  return true;
 }
 
 //———————————————————————————————————————— returns file to save into
@@ -133,7 +98,6 @@ function st_newFile(name) {
 }
 
 //———————————————————————————————————————— options for SVG file
-// accepts boolean multiple artboards · ISG 335
 
 function st_getSvgOptions(){
 
@@ -154,7 +118,7 @@ function st_getSvgOptions(){
   // options.includeVariablesAndDatasets
   // options.optimizeForSVGViewer
   options.preserveEditability = false;                           // Preserve Illustrator Editing Capabilities
-  options.saveMultipleArtboards = false;
+  // options.saveMultipleArtboards
   options.slices = false;                                        // Include Slicing Data
   // options.sVGAutoKerning = true/false;
   options.sVGTextOnPath = false;                                 // Use <textpath> for Text on Path
