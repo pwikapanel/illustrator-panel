ffb362e5a6678cc2c4cccb7f2575222059d844ee
[svijasvg <[login-en@svija.love]>
2021-03-25 17:18:28 +0100

    working image replacement, but some images were not copied to links folder

--- host/resetImages.jsx
+++ host/resetImages.jsx
@@ -2,13 +2,23 @@
 
 var doc = app.activeDocument;
 var placed = doc.placedItems.length;
+var raster = doc.rasterItems.length;
 var treated = 0;
+
+//———————————————————————————————————————— raster images
+// before placed, because will be changed to placed then fixed with others
+
+alert(raster + ' rasterItems to fix');
+for (var x=0; x<raster; x++){
+  treated += rasterItem(doc.rasterItems[0]);
+}
 
 //———————————————————————————————————————— placed images
 
 for (var x=0; x<placed; x++){
-  treated += placedItem(doc.placedItems[x]);
+  treated += placedItem(doc.placedItems[0]);
 }
+
 
 
 
@@ -22,7 +32,7 @@ alert(treated + ' issue(s) were corrected');
 //———————————————————————————————————————— placed images
 
 // https://extendscript.docsforadobe.dev/file-system-access/file-object.html#file-class-properties
-// isg675
+// placedItems isg675
 // app.activeDocument.placedItems.length
 // isg537 app.activeDocument.placedItems[index].blendingMode
 
@@ -60,11 +70,49 @@ function placedItem(obj){
 
 //———————————————————————————————————————— embedded images
 
-// isG749
+// isG749 & isg 561
+// methods 572
 // app.activeDocument.rasterItems.length
+// typename = undefined
+// length = undefined
+// name = ''
 
-function rasterItem(index){
-  alert('embedded, oh no! '+ index);
+// embedded = true
+// file.name = rasterisedimage.png
+
+// obj.file caused crash when file had been deleted
+
+// can relink & copy to links if file exists
+
+// if original file exists, relink it
+// otherwise draw a lime square in same place
+
+// name is what is attributed in layers panel, that's why it's empty
+
+function rasterItem(obj){
+  alert('fixing '+obj.file.fullName); // rasterizedimage.tif
+  var newFile = new File(obj.file.fullName);
+  
+  if(newFile.exists){
+    // get active layer
+
+    newObj = app.activeDocument.placedItems.add()
+    newObj.file = newFile;
+
+    for (var key in obj) {
+      if (key != 'parent' && key != 'embedded' && key != 'wrapOffset' && key != 'wrapInside') newObj[key] = obj[key];
+    }
+    var moveMatrix = app.getScaleMatrix(100,-100);
+    var totalMatrix = concatenateRotationMatrix(moveMatrix, 10);
+    newObj.transform(moveMatrix);
+
+    alert('removing object');
+    obj.remove();
+    alert('object successfully removed');
+    return 1
+  }
+
+  alert('draw lime box over embedded image');
   return 1;
 
 }
