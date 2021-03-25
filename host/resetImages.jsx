e775801f3f58cd532ab58b5eaee7f97e193560fd
[svijasvg <[login-en@svija.love]>
2021-03-25 16:05:30 +0100

    working placed file correction

--- host/resetImages.jsx
+++ host/resetImages.jsx
@@ -1,37 +1,72 @@
 //———————————————————————————————————————— resetImages.jsx
 
 var doc = app.activeDocument;
-var items = doc.pageItems.length;
-var imagesTreated = 0;
+var placed = doc.placedItems.length;
+var treated = 0;
 
-for (x=0; x<items; x++){
+//———————————————————————————————————————— placed images
 
-  arg = doc.pageItems[x].typename;
-
-  switch(arg) {
-    case 'RasterItem': imagesTreated += rasterItem(1); break;
-    case 'PlacedItem': imagesTreated += placedItem(1); break;
-    default: trash = null; break;
-  }
-
+for (var x=0; x<placed; x++){
+  treated += placedItem(doc.placedItems[x]);
 }
 
-//————————————————————————————————————————  fin
 
-alert('Images Re-Linked: '+imagesTreated);
 
-//————————————————————————————————————————  fin
+
+//———————————————————————————————————————— fin
+
+alert(treated + ' issue(s) were corrected');
+
+// — — — — — — — — — — — — — — — — — — — —  functions
+
+//———————————————————————————————————————— placed images
+
+// https://extendscript.docsforadobe.dev/file-system-access/file-object.html#file-class-properties
+// isg675
+// app.activeDocument.placedItems.length
+// isg537 app.activeDocument.placedItems[index].blendingMode
+
+// isg537 for placedItem properties & methods
+
+// PlacedItem.contentVariable
+// PlacedItem.file · readonly
+// app.activeDocument.placedItems[index].selected
+// PlacedItem.uRL
+
+// methods:
+
+// relink: app.activeDocument.placedItems[index].relink(linkFile) returns nothing
+
+// if file notin links folder, can copy it easily
+
+function placedItem(obj){
+  var origFolder = obj.file.path;
+  var goodFolder = Folder(app.activeDocument.path)+'/links';
+  if (origFolder == goodFolder) return 0;
+
+
+  var imgName = obj.file.name;
+  var destFullName = goodFolder+'/'+imgName;
+
+  var newFile = new File(destFullName);
+  if(!newFile.exists){
+    obj.file.copy(newFile);
+  }
+
+  obj.file = newFile;
+
+  return 1;
+}
+
+//———————————————————————————————————————— embedded images
+
+// isG749
+// app.activeDocument.rasterItems.length
 
 function rasterItem(index){
+  alert('embedded, oh no! '+ index);
   return 1;
 
 }
 
-//————————————————————————————————————————  fin
-
-function placedItem(index){
-  alert('placed, oh no!');
-  return 9;
-}
-
-//————————————————————————————————————————  fin
+//———————————————————————————————————————— fin
