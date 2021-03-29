b6b1a8bb4b0a3dd956ebd8f3229f0227ac4032c5
[svijasvg <[login-en@svija.love]>
2021-03-29 15:43:55 +0200

    added all preliminary notes

---
+++ documentation/older/svj 210325 trying to instal zxp locally/SvijaTools/client/indexLinks.js
@@ -0,0 +1,94 @@
+//———————————————————————————————————————— client/indexLinks.js
+
+// link to info pane
+linkInfo.addEventListener('mouseup', callURL);
+function callURL(){ location.href='info.html'; }
+
+//———————————————————————————————————————— reset link
+
+linkLogo.addEventListener('mouseup', refreshPanel);
+function refreshPanel() {
+  csif.evalScript('alert("Reloading Svija Tools");');
+  location.reload;
+}
+
+//———————————————————————————————————————— listeners
+
+linkImages.addEventListener('mouseover', funcMov.bind(null, 'btnImages'), false);
+linkImages.addEventListener('mouseout',  funcMot.bind(null, 'btnImages'), false);
+linkImages.addEventListener('mousedown', funcMod.bind(null, 'btnImages'), false);
+linkImages.addEventListener('mouseup',   funcMou.bind(null, 'btnImages', 'resetImages.jsx', 'null'), false);
+
+linkObjects.addEventListener('mouseover', funcMov.bind(null, 'btnObjects'), false);
+linkObjects.addEventListener('mouseout',  funcMot.bind(null, 'btnObjects'), false);
+linkObjects.addEventListener('mousedown', funcMod.bind(null, 'btnObjects'), false);
+linkObjects.addEventListener('mouseup',   funcMou.bind(null, 'btnObjects', 'resetObjects.jsx', 'null'), false);
+
+linkLayers.addEventListener('mouseover', funcMov.bind(null, 'btnLayers'), false);
+linkLayers.addEventListener('mouseout',  funcMot.bind(null, 'btnLayers'), false);
+linkLayers.addEventListener('mousedown', funcMod.bind(null, 'btnLayers'), false);
+linkLayers.addEventListener('mouseup',   funcMou.bind(null, 'btnLayers', 'propagateLayers.jsx', 'null'), false);
+
+//—————
+
+linkClose.addEventListener('mouseover', funcMov.bind(null, 'btnClose'), false);
+linkClose.addEventListener('mouseout',  funcMot.bind(null, 'btnClose'), false);
+linkClose.addEventListener('mousedown', funcMod.bind(null, 'btnClose'), false);
+linkClose.addEventListener('mouseup',   funcMou.bind(null, 'btnClose', 'saveAsSvg.jsx', 'close'), false);
+
+linkSave.addEventListener('mouseover', funcMov.bind(null, 'btnSave'), false);
+linkSave.addEventListener('mouseout',  funcMot.bind(null, 'btnSave'), false);
+linkSave.addEventListener('mousedown', funcMod.bind(null, 'btnSave'), false);
+linkSave.addEventListener('mouseup',   funcMou.bind(null, 'btnSave', 'saveAsSvg.jsx', 'save'), false);
+
+linkSaveAll.addEventListener('mouseover', funcMov.bind(null, 'btnSaveAll'), false);
+linkSaveAll.addEventListener('mouseout',  funcMot.bind(null, 'btnSaveAll'), false);
+linkSaveAll.addEventListener('mousedown', funcMod.bind(null, 'btnSaveAll'), false);
+linkSaveAll.addEventListener('mouseup',   funcMou.bind(null, 'btnSaveAll', 'saveAsSvg.jsx', 'all'), false);
+
+//———————————————————————————————————————— functions
+
+function funcMov(btnId){
+  var btnHover = document.getElementById(btnId+'Hover' );
+  var btnActive = document.getElementById(btnId+'Active');
+
+  btnHover.style.display = 'block';
+  btnActive.style.display = 'none';
+}
+
+function funcMot(btnId){
+  var btnHover = document.getElementById(btnId+'Hover' );
+  var btnActive = document.getElementById(btnId+'Active');
+
+  btnHover.style.display = 'none';
+  btnActive.style.display = 'none';
+}
+
+function funcMod(btnId){
+  var btnHover = document.getElementById(btnId+'Hover' );
+  var btnActive = document.getElementById(btnId+'Active');
+
+  btnHover.style.display = 'none';
+  btnActive.style.display = 'block';
+}
+
+//———————————————————————————————————————— execution
+
+// var csif = new CSInterface(); // now in index.html
+var path = csif.getSystemPath(SystemPath.EXTENSION) + '/host/';
+
+function funcMou(btnId, scriptName, arg){
+  var btnHover = document.getElementById(btnId+'Hover' );
+  var btnActive = document.getElementById(btnId+'Active');
+
+  btnHover.style.display = 'block';
+  btnActive.style.display = 'none';
+
+  // need to pass argument as well
+
+  file = path + scriptName;
+  csif.evalScript("arg = '" + arg + "'");
+  csif.evalScript("$.evalFile('" + file + "')");
+}
+
+//———————————————————————————————————————— fin
---
+++ documentation/older/svj 210325 trying to instal zxp locally/SvijaTools/host/index.jsx
@@ -0,0 +1,13 @@
+function test(arg){
+  alert(arg);
+//  var fileRef = new File("~/Downloads/kobe.jpg");
+//  var docRef = app.open(fileRef);
+}
+
+//window.addEventListener("keydown", windowKeyDown);
+
+// function windowKeyDown(event){
+//     alert('window KeyHandler: '+event);
+// //    CodeMirror.signal(editor,'catchPanelKeyDown',event);
+// }
+
