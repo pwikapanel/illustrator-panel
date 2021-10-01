28d0288a435dd2acdc5f3d0b36812de4c2cff3f7
[andrewcswift] <[andrew@svija.love]>
2021-10-01 12:55:20 +0200

    all working except gray box in corner

---
+++ Svija Tools/panel/initialize.js
@@ -0,0 +1,20 @@
+//———————————————————————————————————————— title & cookie
+
+var csif = new CSInterface();
+csif.setWindowTitle(title);
+setCookie('advanced', advanced);
+
+//———————————————————————————————————————— set size
+
+var interface_scale = window.__adobe_cep__.getScaleFactor()
+
+function setSize(){
+  var xFactor = Math.round(panel_width/interface_scale);
+  var yFactor = Math.round((panel_height-.5)/interface_scale);
+  csif.resizeContent(xFactor, yFactor);
+  setTimeout(setSize, 500);
+}
+
+setSize();
+
+//———————————————————————————————————————— fin
---
+++ Svija Tools/panel/prev/infoLinks.js
@@ -0,0 +1,7 @@
+//———————————————————————————————————————— client/infoLinks.js
+
+// link to info pane
+linkMain.addEventListener('mouseup', callURL);
+function callURL(){ location.href='index.html'; }
+
+//———————————————————————————————————————— fin
