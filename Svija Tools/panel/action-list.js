c48dc8e290c641f9ce91e4f6e3c9b9c64e28f28c
[andrewcswift] <[andy@svija.com]>
2022-09-29 14:52:10 +0200

    added all bits to repo

---
+++ Svija Tools/panel/action-list.js
@@ -0,0 +1,30 @@
+//———————————————————————————————————————— action-list.js
+
+var actions = [
+
+  // less
+  [''                     , 'less-info.html'         , ''      ],
+  ['Relinking Images…'    , '2. Relink Images.jsx'   , ''      ],
+  ['Saving File…'         , '1. Save as Svija.jsx'   , 'save'  ],
+
+  // more
+  [''                     , 'more-info.html'         , ''      ],
+  ['Resetting ID\'s'      , '5. Reset Object IDs.jsx', ''      ],
+  ['Duplicating Layers…'  , '4. Duplicate Layers.jsx', ''      ],
+  ['Importing Styles…'    , '3. Import Styles.jsx'   , ''      ],
+  ['Relinking Images…'    , '2. Relink Images.jsx'   , ''      ],
+  ['Saving Files…'        , '1. Save as Svija.jsx'   , 'close' ],
+  ['Saving Files…'        , '1. Save as Svija.jsx'   , 'all'   ],
+  ['Saving File…'         , '1. Save as Svija.jsx'   , 'save'  ],
+
+[]];
+
+//    scripts
+//
+//    1. Save for Svija.jsx
+//    2. Relink Images.jsx
+//    3. Import Styles.jsx
+//    4. Duplicate Layers.jsx
+//    5. Reset Object IDs.jsx
+
+//———————————————————————————————————————— fin
