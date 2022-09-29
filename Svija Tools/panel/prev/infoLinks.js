bbb197a5a3eb28b3221b060dba58635aab07c6a9
[andrewcswift] <[andy@svija.com]>
2022-09-29 15:22:58 +0200

    removed extra old stuff

--- Svija Tools/panel/prev/infoLinks.js
+++
@@ -1,7 +0,0 @@
-//———————————————————————————————————————— client/infoLinks.js
-
-// link to info pane
-linkMain.addEventListener('mouseup', callURL);
-function callURL(){ location.href='index.html'; }
-
-//———————————————————————————————————————— fin
