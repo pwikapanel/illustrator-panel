a6547eb285a29fe5e8a2579d8e6667b1592e769b
[andrewcswift] <[andy@svija.com]>
2022-09-29 16:25:05 +0200

    lots of untracked stuff, not sure where it all came from

---
+++ Documentation/older/svj 210325 trying to instal zxp locally/SvijaTools/client/infoLinks 2.js
@@ -0,0 +1,7 @@
+//———————————————————————————————————————— client/indexLinks.js
+
+// link to info pane
+linkInfoPanel.addEventListener('mouseup', callURL);
+function callURL(){ location.href='index.html'; }
+
+//———————————————————————————————————————— fin
