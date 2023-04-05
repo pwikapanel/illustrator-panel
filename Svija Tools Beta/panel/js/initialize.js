125683b929f6783b3742dc359071e8b2545d140e
[andysvija] <[andy@svija.com]>
2023-04-05 13:30:24 +0200

    changed status from checking File to checking Page

--- Svija Tools Beta/panel/js/initialize.js
+++ Svija Tools Beta/panel/js/initialize.js
@@ -36,7 +36,7 @@ const actions = [
   //————— less
 
   [''                     , 'vibe.html?less.html' , ''      ], // top
-  ['Checking File…'       , 'Check.jsx'           , ''      ], // bot
+  ['Checking Page…'       , 'Check.jsx'           , ''      ], // bot
   ['Saving File…'         , 'Save.jsx'            , 'save'  ], // bot
 
   //————— more
@@ -47,7 +47,7 @@ const actions = [
   ['Creating Group…'      , 'Create Group.jsx'    , ''      ], // 2nd
   ['Importing Styles…'    , 'Import Styles.jsx'   , ''      ], // 2nd
 
-  ['Checking File…'       , 'Check.jsx'           , ''      ], // 3rd
+  ['Checking Page……'      , 'Check.jsx'           , ''      ], // 3rd
 
 //['Saving File…'         , 'Save.jsx'            , 'canvas'], // bot
   ['Saving Files…'        , 'Save.jsx'            , 'all'   ], // bot
