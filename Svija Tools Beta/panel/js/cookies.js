b8f203205253d50e3b2798ced03870f571dd6b89
[andysvija] <[andy@svija.com]>
2023-04-11 09:23:37 +0200

    fixed cookie code

--- Svija Tools Beta/panel/js/cookies.js
+++ Svija Tools Beta/panel/js/cookies.js
@@ -5,37 +5,36 @@
     except line 12 commented out */
 
 function setCookie(name, value, expDays) {
-  value = escape(value);
+  value = escape(value)
 
-//deleteParentCookieIfNecessary(name, window.location.hostname);
+//deleteParentCookieIfNecessary(name, window.location.hostname)
 
 //if (expDays > 7) expDays = 7; // max in Safari
 
-  var d = new Date();
-  d.setTime(d.getTime() + (expDays*24*60*60*1000));
+  var d = new Date()
+  d.setTime(d.getTime() + (expDays*24*60*60*1000))
 
-  var expy = '; expires=' + d.toUTCString();
-  var path = '; path=/';
-  var domn = '; domain='  + window.location.hostname;
+  var expy = '; expires=' + d.toUTCString()
+  var path = '; path=/'
+  var domn = '; domain='  + window.location.hostname
 
 //var secu = '; SameSite=Lax; Secure;';
 // secu deprecated: developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
 // var complete = value + expy + path + domn + secu;
 
-  var complete = value + expy + path + domn;
-  document.cookie = name + '=' + complete;
+  var complete = value + expy + path + domn
+  document.cookie = name + '=' + complete
 }
 
 function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(';');
+  var name = cname + "="
+  var ca = document.cookie.split(';')
   for(var i=0; i<ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1);
+    var c = ca[i]
+    while (c.charAt(0)==' ') c = c.substring(1)
     if (c.indexOf(name) != -1){
-      alert('Cookie found: '+cname+'='+c.substring(name.length,c.length));
-      return unescape(c.substring(name.length,c.length));
+      return unescape(c.substring(name.length,c.length))
     }
   }
-  return "";
+  return ""
 }
