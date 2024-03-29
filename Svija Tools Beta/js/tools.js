ea1d5951ed13a9e1b1363cafe85a8f7715be7d9b
[andysvija] <[andy@svija.com]>
2024-03-29 16:11:56 +0100

    working dynamic loader

--- Svija Tools Beta/js/tools.js
+++ Svija Tools Beta/js/tools.js
@@ -32,10 +32,6 @@
 /*:::::::::::::::::::::::::::::::::::::::: control functions */
 
 
-document.body.addEventListener('mouseover', (evn) => {
-  document.body.focus() 
-})
-
 /*———————————————————————————————————————— statusUpdate(arg)
 
     gets info about ai-land and updates
@@ -57,6 +53,8 @@ setInterval(function(){
 function statusUpdateInit(){ jsx.evalScript('statusUpdate("' + localStorage.myDocs + '")', statusUpdate) }
 
 var btnSaveTitle = btnSave.title
+
+const env_path = jsx.getSystemPath(SystemPath.EXTENSION) + encodeURI('/');
 
 function statusUpdate(val){
 
@@ -124,10 +122,56 @@ function statusUpdate(val){
 
 /*:::::::::::::::::::::::::::::::::::::::: indicators */
 
-//   change all to direct function:
-//   indReload.addEventListener('click', (evn) => {
-//     location.reload()
-//   })
+//———————————————————————————————————————— online indicator color matches system
+
+var hue = systemHue()
+var lightness = 50
+var dimness = 30
+
+if (hue>190 && hue < 290) lightness += 10
+
+var highlight = 'hsl('+hue+', 100%, ' + lightness + '%)'
+var  dimlight = 'hsl('+hue+',  30%, ' + dimness   + '%)'
+
+if (typeof localStorage.highlight != 'undefined') highlight = localStorage.highlight
+if (typeof localStorage.dimlight  != 'undefined') dimlight  = localStorage.dimlight
+
+document.documentElement.style.setProperty('--system-highlight', highlight);
+document.documentElement.style.setProperty('--system-dimlight',   dimlight);
+
+/*———————————————————————————————————————— online indicator */
+
+indOnline.addEventListener('mouseup', (evn) => {
+  jsx.evalScript('colorPicker()', setAccent)
+})
+
+// https://community.adobe.com/t5/illustrator-discussions/show-color-picker-dialog-jsx-csaw/td-p/5877341
+
+function setAccent(arg){
+  var parts = arg.split(':')
+
+  var r = parts[0]
+  var g = parts[1]
+  var b = parts[2]
+  
+  var hsl        = rgbToHsl(r, g, b)
+  var hue        = Math.round(hsl[0])
+  var saturation = Math.round(hsl[1])
+  
+  var lightness = 50
+  var dimness   = 30
+  
+  if (hue>190 && hue < 290) lightness += 10
+  
+  var highlight = 'hsl('+hue+', 100%, ' + lightness + '%)'
+  var  dimlight = 'hsl('+hue+',  30%, ' + dimness   + '%)'
+  
+  localStorage.highlight = highlight
+  localStorage.dimlight  = dimlight
+
+  document.documentElement.style.setProperty('--system-highlight', highlight)
+  document.documentElement.style.setProperty('--system-dimlight',   dimlight)
+}
 
 /*———————————————————————————————————————— grid indicator */
 
@@ -237,27 +281,15 @@ function setSmart(swap){
   if (!swap) jsx.evalScript('menuCommand("Snapomatic on-off menu item")')
 }
 
-/*———————————————————————————————————————— local storage FIX AFTER DEBUGGING
-
-    ALSO FIX status-update.jsx
+/*———————————————————————————————————————— local storage
 
     localStorage.removeItem('isSvija') */
 
 dumpLS.addEventListener('click', (evn) => {
 
-  file = env_path + 'preferences-update.jsx'
-  jsx.evalScript("$.evalFile('" + file + "')")
-
-  jsx.evalScript('returnGrid()',lert)
-
-})
-
-
-
-
-
-
-/*
+  localStorage.clear()
+  lert('localStorage cleared')
+  return
 
   var res = 'localStorage contains '+localStorage.length + ' items:\n\n'
 
@@ -270,7 +302,8 @@ dumpLS.addEventListener('click', (evn) => {
     }
   }
 
-  alert(res) */
+  alert(res)
+})
 
 /*———————————————————————————————————————— show more •••
 
@@ -303,11 +336,9 @@ function windowExpand(){
 
     */
 
-createGrp.addEventListener("click", createGroupInit)
-
-function createGroupInit(){
+createGrp.addEventListener("click", (evn) => {
   jsx.evalScript("createGroup()", feedbackSimple)
-}
+})
 
 /*———————————————————————————————————————— 1.2 place image
 
@@ -328,14 +359,14 @@ changeCase.addEventListener('mouseup', (evn) => {
   jsx.evalScript(str, feedbackSimple)
 })
 
-/*———————————————————————————————————————— 1.4 open folder */
+/*———————————————————————————————————————— 1.4 open folder
+
+    */ 
 
 const pcOpener  = 'C:\\Windows\\explorer.exe'
 const macOpener = '/usr/bin/open'
 
-showFolder.addEventListener('mouseup', openFolder);
-
-function openFolder(){
+showFolder.addEventListener('mouseup', (evn) => {
 
   if (localStorage.isMac == 'true') var opener = macOpener
   else                              var opener = pcOpener
@@ -361,7 +392,7 @@ function openFolder(){
 //path = "C:\\Users\\andy\\Desktop\\pixside.fr\\sync\\Example Pages" // worked
   window.cep.process.createProcess(opener, path)
 
-}
+})
 
 /*———————————————————————————————————————— 1.5 launch site & cloud
 
@@ -433,36 +464,21 @@ btnOpen.addEventListener('click', (evn) => {
 
 /*:::::::::::::::::::::::::::::::::::::::: buttons bottom */
 
-/*———————————————————————————————————————— 2.1 check & repair
+/*———————————————————————————————————————— 2.1 import default styles
 
-    */
+*/
 
-chkRepair.addEventListener('click', (evn) => {
-  aiTimeout('checkRepair()', 'feedback', 100)
-})
+/*———————————————————————————————————————— 2.2 set default styles
 
-/*———————————————————————————————————————— 2.2 new page/module
+*/
+
+/*———————————————————————————————————————— 2.3 new page/module
 
     alt = create module instead of page */
 newPage.addEventListener('click', (evn) => {
   var alt   = evn.getModifierState('Alt');
   var doubleLastPath = doubleSlashes(localStorage.lastPath)
   jsx.evalScript('newPage(' + alt + ', "' + doubleLastPath +'",' + localStorage.isMac+')')
-})
-
-/*———————————————————————————————————————— 2.3 new project
-
-    sends an email requesting new project */
-
-newProject.addEventListener('click', (evn) => {
-  var alt = evn.getModifierState('Alt')
-
-  //url = 'mailto:support@svija.com'
-  //url = 'mailto:support@svija.com?subject=Demande de nouveau projet Svija&body=Bonjour\n\nJe voudrais démarrer un nouveau projet Svija à l\'adresse suivante :\n\n       ______.svija.site\n\nCordialement,'
-  url = 'mailto:support@svija.com?subject=Demande de nouveau projet Svija&body=Bonjour\n\nJe voudrais démarrer un nouveau projet Svija à l\'adresse suivante :\n\n       ______.svija.site\n\nCordialement,'
-
-  url = encodeURI(url)
-  jsx.openURLInDefaultBrowser(url)
 })
 
 /*———————————————————————————————————————— 2.4 empty cache NEEDS HELP
@@ -497,7 +513,15 @@ async function testFetch(){
 }
 */
 
-/*———————————————————————————————————————— 2.5 launch support */
+/*———————————————————————————————————————— 2.5 check & repair
+
+    */
+
+chkRepair.addEventListener('click', (evn) => {
+  aiTimeout('checkRepair()', 'feedback', 100)
+})
+
+/*———————————————————————————————————————— 2.6 launch support */
 
 launchSupport.addEventListener('click', (evn) => {
   var alt = evn.getModifierState('Alt')
@@ -521,19 +545,20 @@ buttons = [
 
 //  id               label      alt-label      tooltip
 
-  ['createGrp'    , 'Groupe'  , 'Groupe'   , 'Créer un groupe'                                                       ],
-  ['placeImage'   , 'Image'   , 'Image'    , 'Importer une image'                                                    ],
-  ['changeCase'   , 'Casse'   , 'Mot mot'  , 'Changer la casse · alt = Premières Lettres En Maj'                     ],
-  ['showFolder'   , 'Dossier' , 'Dossier'  , 'Ouvrir le dossier local'                                               ],
-  ['launchSite'   , 'Site'    , 'Cloud'    , 'Ouvrir le site web · alt = ouvrir Svija Cloud'                         ],
-  ['btnSave'      , 'Sauv.'   , 'Tous'     , 'Sauvegarder la page · alt = sauvegarder toutes les pages ouvertes'     ],
-  ['btnOpen'      , 'Ouvrir'  , 'Récent'   , 'Ouvrir un ficher · alt = ouvrir le fichier le plus récent'             ],
+  ['createGrp'    , 'Groupe'  , 'Groupe'   , 'Créer un groupe'                                                              ],
+  ['placeImage'   , 'Image'   , 'Image'    , 'Importer une image'                                                           ],
+  ['changeCase'   , 'Casse'   , 'Mot mot'  , 'Changer la casse · alt/option = Premières Lettres En Maj'                     ],
+  ['showFolder'   , 'Dossier' , 'Dossier'  , 'Ouvrir le dossier local'                                                      ],
+  ['launchSite'   , 'Site'    , 'Cloud'    , 'Ouvrir le site web dans un navigateur (alt/option = Svija Cloud)'             ],
+  ['btnSave'      , 'Sauv.'   , 'Tous'     , 'Sauvegarder la page · alt/option = sauvegarder toutes les pages ouvertes'     ],
+  ['btnOpen'      , 'Ouvrir'  , 'Récent'   , 'Ouvrir un ficher · alt/option = ouvrir le fichier le plus récent'             ],
 
-  ['chkRepair'    , 'Vérifier', 'Vérifier'  , 'Vérifier & réparer les problèmes'                                      ],
-  ['newPage'      , '+Page'   , 'Module'   , 'Nouvelle page · alt = nouveau module'                                  ],
-  ['newProject'   , '+Projet' , '+Projet'  , 'Créer un nouveau projet Svija'                                         ],
-  ['clearCache'   , 'Cache'   , 'Cache'    , 'Vider le cache (si activé)'                                            ],
-  ['launchSupport', 'Support' , 'Mail'     , 'Ouvrir le site de support technique · alt = envoyer un mail au support']
+  ['impStyles'    , '^Styles' , '^Styles'  , 'Importer styles par défaut'                                                   ],
+  ['expStyles'    , 'vStyes'  , 'vStyes'   , 'Établir comme styles par défaut'                                              ],
+  ['newPage'      , '+Page'   , 'Module'   , 'Nouvelle page · alt/option = nouveau module'                                  ],
+  ['clearCache'   , 'Cache'   , 'Cache'    , 'Vider le cache (si activé)'                                                   ],
+  ['chkRepair'    , 'Vérifier', 'Vérifier' , 'Vérifier & réparer les problèmes'                                             ],
+  ['launchSupport', 'Support' , 'Mail'     , 'Ouvrir le site de support technique · alt/option = envoyer un mail au support']
 ]
 
 function initializeLabels(){
@@ -548,80 +573,6 @@ function initializeLabels(){
 
 initializeLabels()
 
-/*———————————————————————————————————————— alt key listeners
-
-    get information about alt key state
-    alt is keyCode 18  */
-
-document.body.addEventListener('mouseover', (evn) => {
-  document.body.focus() 
-})
-
-document.body.addEventListener('mouseover', buttonLabels)
-document.body.addEventListener('mouseout' , buttonLabels)
-document.body.addEventListener('mousemove', buttonLabels)
-document.body.addEventListener('mouseup'  , buttonLabels)
-document.body.addEventListener('mousedown', buttonLabels)
-document.body.addEventListener('onkeydown', buttonLabels)
-
-/*———————————————————————————————————————— buttonLabels(evn)
-
-    alt text should show only when cursor is over panel
-    whether or not alt is pressed before entering airspace
-
-    when mouse moves away from panel it should revert
-
-    the only tricky part is when the mouse moves away from
-    the panel but panel has focus — alt key needs
-    to stop having effect */
-
-// area = flag that alt/title have been swapped
-
-
-function buttonLabels(evn){
-  var alt   = evn.getModifierState('Alt');
-
-  if (alt) // set to alt state
-    for (var x=0; x<buttons.length; x++)
-      window[buttons[x][0]].value = window[buttons[x][0]].alt
-
-  else // set to normal state
-    for (var x=0; x<buttons.length; x++)
-      window[buttons[x][0]].value = window[buttons[x][0]].name
-
-}
-
-/*———————————————————————————————————————— mouse over window COMMENTED OUT
-
-    store info about mouse over window state
-    and alt key if it happened before window
-    got focus */
-
-/*
-window.addEventListener('mouseover', altOver )
-window.addEventListener('mouseout',  altOut  )
-
-localStorage.isOver = false
-
-function altOver(event) {
-  jsx.registerKeyEventsInterest(keyStr)
-  localStorage.isOver = true
-
-  // handle case where alt key was pressed
-  // before panel was mouseovered
-  if (event.altKey){
-    localStorage.altDown = true
-    buttonLabels(true)
-  }
-}
-
-function altOut(event) {
-  jsx.registerKeyEventsInterest(null)
-  localStorage.isOver = false
-  buttonLabels(false)
-}
-*/
-
 /*———————————————————————————————————————— tooltips
 
     need to set a delay and if still over, show tooltip
@@ -630,13 +581,12 @@ function altOut(event) {
     a document is open (changes from save to open)  */
 
 var ttText  = mouseover.innerHTML
-var ttDelay = 1500
+var ttDelay = 1000
 
-//r targetList = ['indOnline',  'indGrid',    'indGuide', 'indPoint', 'indPixel', 'indSmart', 'indReload', 'dumpLS',
-var targetList = ['indOnline',  'indGrid',    'indGuide',                         'indSmart', 'indReload', 'dumpLS',
-                  'btnSave',    
-                  'placeImage', 'changeCase', 'launchSite', 'showFolder', 'createGrp',
-                  'chkRepair',  'clearCache', 'newPage', 'newProject', 'launchSupport']
+var targetList = ['indOnline',  'indGrid',    'indGuide', 'indSmart', 'indReload', 'dumpLS',
+                  'createGrp', 'placeImage', 'changeCase', 'showFolder', 'launchSite', 'btnSave',    
+                  
+                  'chkRepair',  'clearCache', 'newPage',  'launchSupport']
 
 for (x=0; x<targetList.length; x++){
   window[targetList[x]].addEventListener('mouseover', function(e){
@@ -657,6 +607,70 @@ function showTip(objName){
     mouseover.style.color = 'var(--tooltipOn)'
   }
 }
+
+/*———————————————————————————————————————— mouse over window COMMENTED OUT
+
+    store info about mouse over window state
+    and alt key if it happened before window
+    got focus */
+
+// proved: mouseover/mouseout triggered on document.body even when there's no focus
+// proved: I can focus() an input field only if the panel already has focus
+
+/*
+window.addEventListener('mouseover', (evn) => {
+  document.body.style.backgroundColor="#550000"
+  testInput.focus()
+  //alert(evn)
+})
+
+window.addEventListener('mouseout'  , (evn) => {
+  document.body.style.backgroundColor="#444444"
+  testInput.blur()
+  //alert(evn)
+})
+
+var keyStr = 18 // option on mac
+jsx.registerKeyEventsInterest(keyStr)
+
+document.body.addEventListener('keydown', (evn) => {
+  alert(evn)
+})
+
+document.body.addEventListener('keyup'  , (evn) => {
+  alert(evn)
+})
+*/
+
+/*———————————————————————————————————————— buttonLabels(evn) COMMENTED OUT
+
+    alt text should show only when cursor is over panel
+    whether or not alt is pressed before entering airspace
+
+    when mouse moves away from panel it should revert
+
+    the only tricky part is when the mouse moves away from
+    the panel but panel has focus — alt key needs
+    to stop having effect */
+
+// area = flag that alt/title have been swapped
+
+/*
+function buttonLabels(evn){
+  var alt   = evn.getModifierState('Alt');
+
+function buttonLabels(alt){
+
+  if (alt) // set to alt state
+    for (var x=0; x<buttons.length; x++)
+      window[buttons[x][0]].value = window[buttons[x][0]].alt
+
+  else // set to normal state
+    for (var x=0; x<buttons.length; x++)
+      window[buttons[x][0]].value = window[buttons[x][0]].name
+
+}
+*/
 
 
 /*:::::::::::::::::::::::::::::::::::::::: utility functions */
@@ -724,17 +738,57 @@ function feedback(arg){
     doneMsg.style.display = 'none'
   }
 
-/*———————————————————————————————————————— lert(msg)
+/*———————————————————————————————————————— rgbToHsl(r, g, b)
 
-    alerts in ai-land don't exit program space */
+    https://www.30secondsofcode.org/js/s/rgb-to-hsl/  */
 
-function lert(msg){
-  msg = JSON.stringify(String(msg));
-  msg = msg.substr(1, msg.length-2)
-  //alert(msg)
-  jsx.evalScript('alert("' + msg + '")')
+function rgbToHsl(r, g, b){
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  const l = Math.max(r, g, b);
+  const s = l - Math.min(r, g, b);
+  const h = s
+    ? l === r
+      ? (g - b) / s
+      : l === g
+      ? 2 + (b - r) / s
+      : 4 + (r - g) / s
+    : 0;
+  return [
+    60 * h < 0 ? 60 * h + 360 : 60 * h,
+    100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
+    (100 * (2 * l - s)) / 2,
+  ];
 }
 
+/*———————————————————————————————————————— systemHue
+
+    returns hue of system highlight color, except...
+
+    - if it's windows default blue, we return Svija color
+    - if it's less than 10% saturated we return Svija color */
+
+function systemHue(){
+
+  var hostEnv     = jsx.getHostEnvironment();
+  var RGBColorObj = hostEnv.appSkinInfo.systemHighlightColor;
+  
+  var r = RGBColorObj.red
+  var g = RGBColorObj.green
+  var b = RGBColorObj.blue
+  
+  var hsl        = rgbToHsl(r, g, b)
+  var hue        = Math.round(hsl[0])
+  var saturation = Math.round(hsl[1])
+  
+  if (''+r+g+b == '0120215') hue = 72
+  
+  if (saturation < 10) hue = 72   // if system highlight is gray, use official color
+  
+  return hue
+}
+  
 
 /*:::::::::::::::::::::::::::::::::::::::: end */
 
