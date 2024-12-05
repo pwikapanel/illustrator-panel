0e9f15d19f1b1d1777fe1e3a8a7afff364a383a7
[acswift] <[andy@svija.com]>
2024-12-05 11:47:02 +0100

    status window closes with click, escape, or button

--- Beta/master/jsx/showAlert.jsx
+++ Beta/master/jsx/showAlert.jsx
@@ -19,38 +19,54 @@
 
 function showAlert(arg){
 
-  //———————————————————— create panel
+  //———————————————————— create palette
 
-  panel = new Window ('palette', 'Alert', undefined, {resizeable: false, borderless: true, closeButton: false})
-  panel.graphics.backgroundColor = panel.graphics.newBrush (panel.graphics.BrushType.SOLID_COLOR, [0.2, 0.2, 0.2])
+  palette = new Window ('palette', 'Alert', undefined, {resizeable: false, borderless: true, closeButton: false})
+  palette.graphics.backgroundColor = palette.graphics.newBrush (palette.graphics.BrushType.SOLID_COLOR, [0.2, 0.2, 0.2])
 
-  panel.preferredSize = [400, 60] // +28px where title bar was
-  panel.margins       = [0, 0, 0, 0]
-  panel.orientation   = 'row'
-  panel.alignChildren = ['fill', 'fill']
-  panel.spacing=0
+  palette.preferredSize = [400, 60] // +28px where title bar was
+  palette.margins       = [0, 0, 0, 0]
+  palette.orientation   = 'row'
+  palette.alignChildren = ['fill', 'fill']
+  palette.spacing=0
+  
+  //———————————————————— alert text
 
-  var content = panel.add ('group');
-  content.spacing=0
-  content.orientation   = 'column'
-  content.preferredSize = [310,292]
-  content.margins = [0, 0, 0, 0]
-  content.graphics.backgroundColor = content.graphics.newBrush(content.graphics.BrushType.SOLID_COLOR,[0.7,0.7,0.7], 1);
-  content.addEventListener("click", function(e){ panel.hide() })
+  var para = palette.add('group')
+  para.margins = [0, 28, 30, 0]
+  para.alignment = 'center'
+  para.orientation = 'column'
+  para.spacing   = 2
+  para.graphics.backgroundColor = para.graphics.newBrush(para.graphics.BrushType.SOLID_COLOR,[0.3, 0.3, 0.3], 1)
 
-  //———————————————————— paragraph
 
-  var  paraLine1 = content.add("statictext")
+  para.addEventListener('click', function(e){ palette.hide() })
+
+  var  paraLine1 = para.add("statictext")
 
   paraLine1.graphics.foregroundColor = paraLine1.graphics.newPen (paraLine1.graphics.PenType.SOLID_COLOR, [0.75, 0.75, 0.75], 1);
 
   paraLine1.text = arg
 
 
+  //———————————————————— cancel & apply buttons
 
+  cancelButton = palette.add("button", undefined, "Close")
+//  applyButton  = palette.add("button", undefined, "Apply")
 
-  if(panel.show() == 1){} 
-  else return ''
+  cancelButton.alignment = ['', 'fill'] 
+//  applyButton.alignment  = ['', 'fill']  // permits smaller buttons
+
+  //———————————————————— button functionality
+
+  palette.cancelElement = cancelButton
+
+  cancelButton.onClick = function(e){
+    palette.hide()
+  }
+
+  if(palette.show() == 1){} 
+  else return ''                                     // clicked cancel
 
 }
 
