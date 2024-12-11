1e6e62573f9405eb2e8a2b6981b40c3c277255ee
[acswift] <[andy@svija.com]>
2024-12-11 12:14:18 +0100

    starting work on loading process

---
+++ Beta/master/js/version.js
@@ -0,0 +1,35 @@
+
+/*:::::::::::::::::::::::::::::::::::::::: version.js */
+
+// var AIVERSIONMIN  = 26.0 // in shell.js
+
+
+//———————————————————————————————————————— clean up version
+
+
+var parts  = AIVERSION.split('.')
+var points = parts.length - 1
+
+if (points > 1)
+  var vers = parts[0] + '.' + parts[1]
+else
+  var vers = AIVERSION
+
+vers = parseFloat(vers)
+
+//———————————————————————————————————————— is it useable?
+
+if (vers < AIVERSIONMIN){
+  versionDiv.style.display = 'block'
+  versionBanner.innerHTML = translate('versionAlert')
+
+  versionBanner.addEventListener('mouseup', (evn) => {
+    var url = 'https://tech.svija.love/illustrator-version'
+    CEP.openURLInDefaultBrowser(url)
+    return true
+  })
+
+}
+
+/*:::::::::::::::::::::::::::::::::::::::: fin */
+
