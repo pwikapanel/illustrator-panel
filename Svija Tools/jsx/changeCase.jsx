33666fb940c05fb22f611858f2bfdd4a865ef91a
[acswift] <[andy@svija.com]>
2025-06-20 15:45:50 +0200

    start with function getTextMulti(sel)

--- Svija Tools/jsx/changeCase.jsx
+++ Svija Tools/jsx/changeCase.jsx
@@ -2,6 +2,25 @@
 
 //:::::::::::::::::::::::::::::::::::::::: changeCase.jsx
 
+/* remove groups —————————————————————————————————————————————————————————————
+
+    this should only happen if group length is 1
+
+    while (selection[0] instanceof GroupItem){
+      app.activeDocument.selection = selection[0].pageItems[0]
+      var selection = app.activeDocument.selection;
+      alert('Group Removed')
+    }
+  
+    // selected item is not text ———————————————————————————————————————————————
+  
+    if (!selectedChars && !(selection[0] instanceof TextFrame)){
+      alert('Please select text')
+      return true
+    }
+
+    I don't need to return an error, because there is no alert anyway */
+
 /*———————————————————————————————————————— notes
 
      needs to work with
@@ -20,65 +39,31 @@
      • some characters selected with cursor
      • objects with text selected */
 
-/*———————————————————————————————————————— (c) & EULA
-
-    Copyright (c) 2019-2023 Svija
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-    
-    The software is provided "as is", without warranty of any kind, express or
-    implied, including but not limited to the warranties of merchantability,
-    fitness for a particular purpose and noninfringement. In no event shall the
-    authors or copyright holders be liable for any claim, damages or other
-    liability, whether in an action of contract, tort or otherwise, arising from,
-    out of or in connection with the software or the use or other dealings in
-    the software.
-
-  	svija.com · hello@svija.com */
-
 
 //:::::::::::::::::::::::::::::::::::::::: program
 
 function changeCase(alt){
 
-  // if no selection, do nothing ———————————————————————————————————————————————
+  // if no file or selection, do nothing ———————————————————————————————————————————————
 
   if (app.documents.length < 1)  return ''
   if (app.activeDocument.selection.length < 1) return ''
 
-  // variable declarations —————————————————————————————————————————————————————
-
-  var textRange     // is it the entire path (vs selected with cursor)
-  var selection     // active selection at a given moment
-  var initSelection // what was selected when script was called
-
-  // get selection —————————————————————————————————————————————————————————————
+  // get selection & type of selection —————————————————————————————————————————————————————————————
 
   var selection = app.activeDocument.selection;
 
-  // is it selected characters? ————————————————————————————————————————————————
-  
   if (selection instanceof TextRange)
-    textRange = true
+    selectedChars = true
   else
-    textRange = false
+    selectedChars = false
 
   // get selected text —————————————————————————————————————————————————————————
 
-  if (textRange)
-    var allChars = getTextRange(selection)
+  if (selectedChars)
+    var allChars = selection.contents
   else
-    var allChars = getTextMulti(selection)
-
-  alert('83: '+allChars)
+    var allChars = getTextMulti(selection) ////////////////////////////////////////////////////////// THIS IS WHERE PROBLEM STARTS
 
   // is first letter upper or lower? ———————————————————————————————————————————
 
@@ -113,15 +98,7 @@ function changeCase(alt){
 
 //:::::::::::::::::::::::::::::::::::::::: functions
 
-/*———————————————————————————————————————— getTextRange(selection)
-
-     */
-
-function getTextRange(selection){
-  return selection.contents
-}
-
-/*———————————————————————————————————————— getTextMulti(sel)
+/*———————————————————————————————————————— getTextMulti(sel) /////////////////////////////////////// START WORK HERE
 
     need to iterate through every element of selection
     need all text, because it could be lots of numbers followed by a letter
@@ -176,23 +153,4 @@ function getTextObj(selection){
 
 
 //:::::::::::::::::::::::::::::::::::::::: fin
-
-  // remove groups —————————————————————————————————————————————————————————————
-
-// this should only happen if group length is 1
-
-//while (selection[0] instanceof GroupItem){
-//  app.activeDocument.selection = selection[0].pageItems[0]
-//  var selection = app.activeDocument.selection;
-//  alert('Group Removed')
-//}
-
-  // selected item is not text ———————————————————————————————————————————————
-
-//if (!textRange && !(selection[0] instanceof TextFrame)){
-//  alert('Please select text')
-//  return true
-//}
-
-// I don't need to return an error, because there is no alert anyway
 
