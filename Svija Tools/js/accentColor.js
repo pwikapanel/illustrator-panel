0f839c7cc799bbd0750e516e080ed27df209b6c7
[acswift] <[andy@svija.com]>
2025-06-20 10:59:06 +0200

    break time

--- Svija Tools/js/accentColor.js
+++ Svija Tools/js/accentColor.js
@@ -102,6 +102,30 @@ function dimVersion(hue){
   return 'hsl('+hue+', 20%, ' + lightness + '%)'
 }
 
+/*———————————————————————————————————————— ut_rgbToHsl(r, g, b)
+
+    https://www.30secondsofcode.org/js/s/rgb-to-hsl/  */
+
+function ut_rgbToHsl(r, g, b){
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  const l = Math.max(r, g, b);
+  const s = l - Math.min(r, g, b);
+  const h = s
+    ? l === r
+      ? (g - b) / s
+      : l === g
+      ? 2 + (b - r) / s
+      : 4 + (r - g) / s
+    : 0;
+  return [
+    60 * h < 0 ? 60 * h + 360 : 60 * h,
+    100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
+    (100 * (2 * l - s)) / 2,
+  ];
+}
+
 
 /*:::::::::::::::::::::::::::::::::::::::: fin */
 
