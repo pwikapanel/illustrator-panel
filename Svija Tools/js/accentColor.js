a1bfcbe0009400b8341ef73319a064b8e210c3c4
[acswift] <[andy@svija.com]>
2025-06-20 15:34:49 +0200

    found color error

--- Svija Tools/js/accentColor.js
+++ Svija Tools/js/accentColor.js
@@ -1,19 +1,6 @@
 
 /*:::::::::::::::::::::::::::::::::::::::: accentColor.js */
 
-/*———————————————————————————————————————— logo SVG code
-
-/><svg id="svijaLogo"
-  onmouseover="logoArt.style.transform='scale(1.1)'"
-  onmouseout ="logoArt.style.transform='scale(1.0)'"
-  xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 105 22">
-  <g>
-    <rect id="logoBar" class="cls-1" y="7.85" width="105" height="6.3"/>
-    <path id="logoArt" style="transform-origin: 50% 50%;"
-      d="M62.133,5.571h-2.571v11.143h2.571V5.571ZM66.705,1h-7.143v2.571h7.143V1ZM66.705,5.571h-2.571v15.429h2.571V5.571ZM36.295,16.714h-16.857v-2.571h14.286v-.994l-14.286-1.735v-5.843h16.857v2.571h-14.286v.994l14.286,1.735v5.843ZM85.562,5.571h-16.857v2.571h14.286v.994l-14.286,1.735v5.843h16.857V5.571ZM71.276,13.149l11.714-1.423v2.416h-11.714v-.994ZM55.614,5.571l-5.289,8.178c-.159.246-.43.394-.723.394h-3.38c-.293,0-.564-.147-.723-.394l-5.289-8.178h-3.062l6.192,9.574c.635.982,1.713,1.569,2.882,1.569h3.38c1.17,0,2.247-.586,2.882-1.569l6.192-9.574h-3.062Z"/>
-  </g>
-</svg */
-
 /*———————————————————————————————————————— store colors in localStorage
 
     if user already picked a favorite color */
@@ -31,14 +18,9 @@ else{
   document.documentElement.style.setProperty('--accentDim',    localStorage.accentDim)
 }
 
-elapse(27, 'localStorage.accentBright = ' + localStorage.accentBright)
-elapse(28, 'localStorage.accentDim    = ' + localStorage.accentDim)
-
 /*———————————————————————————————————————— online status color
 
     colored if online, red bar if offline */
-
-elapse(41, 'navigator.onLine='+navigator.onLine)
 
 onlineStatus()
 setInterval(onlineStatus, INTMS)
@@ -46,19 +28,15 @@ setInterval(onlineStatus, INTMS)
 function onlineStatus(){
 
   if (navigator.onLine){
-    elapse(49, 'navigator.onLine='+navigator.onLine+', setting normal colors')
     logoArt.style.fill = 'var(--accentBright)'
     logoBar.style.fill = 'var(--panel-bg-dark)'
   }
   
   else{
-    elapse(55, 'navigator.onLine='+navigator.onLine+', setting offline colors')
     logoArt.style.fill = 'none'
     logoBar.style.fill = 'red'
   }
 
-  elapse(57, 'logoArt colors set to '+logoArt.style.fill)
-  elapse(58, 'logoBar colors set to '+logoBar.style.fill)
 }
 
 /*———————————————————————————————————————— svijaLogo.addEventListener('mouseup'
